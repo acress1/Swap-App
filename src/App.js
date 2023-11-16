1c0027c71305a570971db14441147f47177b52a7
Matheus <MathieuBk0@gmail.com>
2023-11-16 19:17:51 +0000

    2nd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import InlineForm from './components/InlineForm/InlineForm';
+import Calendar from './components/Calendar/Calendar';
 import './App.css';
 
 function App() {
+  const todayDate = new Date().toDateString();
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <InlineForm todayDate= {todayDate} timeZone= {timeZone} />
     </div>
+    <div>
+      <Calendar />
+    </div>
+    </>
   );
 }
 
