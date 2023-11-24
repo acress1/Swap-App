80a53563d9c457e8555ece211e13c35b089825dc
Matheus <MathieuBk0@gmail.com>
2023-11-24 17:46:37 +0000

    Added multi submissions

--- src/components/InlineForm/InlineForm.js
+++ src/components/InlineForm/InlineForm.js
@@ -2,17 +2,10 @@ import React from "react";
 import './InlineForm.css';
 
 const InlineForm = ({ todayDate, timeZone }) => {
-  const [shifts, setShifts] = React.useState([{setIsOvernight: false}]);
-  const isOvernight = React.useState(false);
-
-  const ovSwitch = (index) => { 
-    const updatedShifts = [...shifts];
-    updatedShifts[index].isOvernight = !updatedShifts[index].isOvernight;
-    setShifts(updatedShifts)
-  };
+  const [shifts, setShifts] = React.useState([{isOvernight: false, Date: '', Outbound: '', Inbound: '', FIRST: false, BAR: false, Early: false, Late: false, LTA: false, DO: false}]);
 
   const addShift = () => {
-    const newShifts = [...shifts, {isOvernight: false}];
+    const newShifts = [...shifts, {isOvernight: false, Date: '', Outbound: '', Inbound: '', FIRST: false, BAR: false, Early: false, Late: false, LTA: false, DO: false}];
     setShifts(newShifts)
   };
 
@@ -22,51 +15,62 @@ const InlineForm = ({ todayDate, timeZone }) => {
     setShifts(updatedShifts);
   };
 
+  const ovSwitch = (index) => { 
+    const updatedShifts = [...shifts];
+    updatedShifts[index].isOvernight = !updatedShifts[index].isOvernight;
+    setShifts(updatedShifts)
+  };
+
+  const handleChange = (index, fieldName, value) => {
+    const updatedShifts = [...shifts];
+    updatedShifts[index][fieldName] = value;
+    setShifts(updatedShifts);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const formData = {
-      Name: e.target.elements.Name.value,
-      Email: e.target.elements.Email.value,
-      Date: e.target.elements.Date.value,
-      Outbound: e.target.elements.Outbound.value,
-      Inbound: isOvernight ? e.target.elements.Inbound.value + '+1d' : e.target.elements.Inbound.value,
-      FIRST: e.target.elements.FIRST.checked,
-      BAR: e.target.elements.BAR.checked,
-      Early: e.target.elements.Early.checked,
-      Late: e.target.elements.Late.checked,
-      LTA: e.target.elements.LTA.checked,
-      DO: e.target.elements.DO.checked,
-      Note: e.target.elements.Note.value
-    };
-
-    const requiredFields = ['Name','Email','Date','Outbound','Inbound'];
-    
-    const missingFields = requiredFields.filter(field => !formData[field]);
-
-    if (missingFields.length > 0) {
+    const isValid = shifts.every(shift => shift.Date && shift.Outbound && shift.Inbound);
+    if (!isValid) {
       alert('Oops... Something\'s missing 🤓');
+      return;
     }
 
-    fetch('http://localhost:3001/formData', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(formData)
+    shifts.forEach((shift, index) => {
+      const formData = {
+        Name: e.target.elements.Name.value,
+        Email: e.target.elements.Email.value,
+        Date: shift.Date,
+        Outbound: shift.Outbound,
+        Inbound: shift.isOvernight ? shift.Inbound + '+1d' : shift.Inbound,
+        FIRST: shift.FIRST,
+        BAR: shift.BAR,
+        Early: shift.Early,
+        Late: shift.Late,
+        LTA: shift.LTA,
+        DO: shift.DO,
+        Note: e.target.elements.Note.value
+      };
+
+      fetch('http://localhost:3001/formData', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+        })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Form submission failed');
+        }
+        return response.json()
       })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Form submission failed');
-      }
-      return response.json()
-    })
-    .then(data => {
-      console.log('Success', data);
-      alert('Form submitted successfully!')
-    })
-    .catch(error => {
-      console.log(error);
+      .then(data => {
+        console.log('Success', formData);
+        alert('Form submitted successfully!')
+      })
+      .catch(error => {
+        console.log(error);
+      });
     });
-    setShifts([{key: 0}])
   };
   
   return (
@@ -80,21 +84,21 @@ const InlineForm = ({ todayDate, timeZone }) => {
           </div>
             {shifts.map((shift, index) => (
               <div className="shift" key={index}>
-                <input type="date" name="Date" />
-                <input type="number" min="9000" max="9199" name="Outbound" placeholder="Outbound" />
-                <input type="number" min="9000" max="9199" name="Inbound" placeholder="Inbound" />
+                <input type="date" name="Date" value={shift.Date} onChange={(e) => handleChange(index, 'Date', e.target.value)} />
+                <input type="number" min="9000" max="9199" name="Outbound" placeholder="Outbound" value={shift.Outbound} onChange={(e) => handleChange(index, 'Outbound', e.target.value)} />
+                <input type="number" min="9000" max="9199" name="Inbound" placeholder="Inbound" value={shift.Inbound} onChange={(e) => handleChange(index, 'Inbound', e.target.value)} />
                 <label>OVERNIGHT</label>
                 <label className="switch">
                   <input type="checkbox" onChange={() => ovSwitch(index)} checked={shift.isOvernight} />
                   <span className="slider round"></span>
                 </label>
-                <label>FIRST<input type="checkbox" name="FIRST" /></label>
-                <label>BAR<input type="checkbox" name="BAR" /></label>
+                <label>FIRST<input type="checkbox" name="FIRST" checked={shift.FIRST} onChange={(e) => handleChange(index, 'FIRST', e.target.checked)} /></label>
+                <label>BAR<input type="checkbox" name="BAR" checked={shift.BAR} onChange={(e) => handleChange(index, 'BAR', e.target.checked)} /></label>
                 <span> LOOKING FOR : </span>
-                <label>Early<input type="checkbox" name="Early" /></label>
-                <label>Late<input type="checkbox" name="Late" /></label>
-                <label>LTA<input type="checkbox" name="LTA" /></label>
-                <label>Day OFF<input type="checkbox" name="DO" /></label>
+                <label>Early<input type="checkbox" name="Early" checked={shift.Early} onChange={(e) => handleChange(index, 'Early', e.target.checked)} /></label>
+                <label>Late<input type="checkbox" name="Late" checked={shift.Late} onChange={(e) => handleChange(index, 'Late', e.target.checked)} /></label>
+                <label>LTA<input type="checkbox" name="LTA" checked={shift.LTA} onChange={(e) => handleChange(index, 'LTA', e.target.checked)} /></label>
+                <label>Day OFF<input type="checkbox" name="DO" checked={shift.DO} onChange={(e) => handleChange(index, 'DO', e.target.checked)} /></label>
                 <button className="add-line" type="button" onClick={addShift}></button>
                 <button className="delete-line" type="button" onClick={() => deleteShift(index)}></button>
               </div>
