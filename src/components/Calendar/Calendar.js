33975bdb705c9319c267ffb773e116148f662d2d
Matheus <MathieuBk0@gmail.com>
2023-12-09 17:06:47 +0000

    Removed Titles

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -44,8 +44,7 @@ const Calendar = () => {
           <div className="calendar">
             {eachDayOfInterval({ start: startOfMonth(month), end: endOfMonth(month) }).map(day => (
               <div 
-                key={day} 
-                title='Click here to see what colleagues are willing to swap on this day 🤓' 
+                key={day}
                 className={`calendar-day
                   ${selectedDay && format(day, 'MMMM dd yyyy') === format(selectedDay, 'MMMM dd yyyy') ? 'calendar-day-selected' : ''}`} 
                 onClick={() => toggleSelectedDay(day)}
