da03348c994b9450733a620772c04d855381ae2b
Matheus <MathieuBk0@gmail.com>
2024-01-10 14:36:29 +0000

    Added component test files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import { format } from 'date-fns';
 import { ToastContainer, toast } from "react-toastify";
 import InlineForm from './components/InlineForm/InlineForm';
