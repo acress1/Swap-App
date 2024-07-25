d5f8d9936ad49d90cc2c1021f2fb30dd3c342a03
Matheus <MathieuBk0@gmail.com>
2024-07-25 20:12:58 +0100

    Pre-TypeScript commit

--- src/components/InlineForm/LinkedButtons.js
+++ src/components/InlineForm/LinkedButtons.js
@@ -3,24 +3,24 @@ const LinkedButtons = () => {
     
     return(
         <div className="linked-button-grid">
-            <a 
+            <button 
                 className="swap-form-button" 
-                href="https://app.smartsheet.com/b/form/20d18963576e477bafcbf102df2aec3d" 
-                target="_blank" 
-                rel="noreferrer"
-                >Swap Form</a>
-            <a 
+                onClick={() => window.open("https://app.smartsheet.com/b/form/20d18963576e477bafcbf102df2aec3d", "_blank", "noreferrer")}
+            >
+                Swap Form
+            </button>
+            <button 
                 className="roster-button" 
-                href="https://www.momentumserviceslondon.com/activite" 
-                target="_blank" 
-                rel="noreferrer"
-                >Roster</a>
-            <a 
+                onClick={() => window.open("https://www.momentumserviceslondon.com/activite", "_blank", "noreferrer")}
+            >
+                Roster
+            </button>
+            <button 
                 className="tutorial-button" 
-                href="https://youtu.be/lGQ-xiyTrCk" 
-                target="_blank" 
-                rel="noreferrer"
-                >Tutorial</a>
+                onClick={() => window.open("https://youtu.be/lGQ-xiyTrCk", "_blank", "noreferrer")}
+            >
+                Tutorial
+            </button>
         </div>
     );
 };
