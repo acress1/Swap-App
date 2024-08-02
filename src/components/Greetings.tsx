f314273b4b35eaa11f56639de88b83a63fd92022
Matheus <MathieuBk0@gmail.com>
2024-08-02 19:23:30 +0100

    Adding conditional to Newsbutton rendering

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -7,6 +7,9 @@ const Greetings = ({ todayDate }: { todayDate: Date }) => {
     const [showNewsBox, setShowNewsBox] = useState(false);
     const formatedTodayDate = format(todayDate, 'MMMM do, y O');
 
+    const newsBoxMessage = "";
+    const hasMessage = newsBoxMessage.length > 0;
+
     const toggleNewsBox = () => {
         setShowNewsBox(!showNewsBox);
     };
@@ -19,11 +22,16 @@ const Greetings = ({ todayDate }: { todayDate: Date }) => {
                 alt="🤓"
             />
             <br />
-            <button
-                className="newsButton"
-                onClick={toggleNewsBox}
-            > Last update </button>
-            {showNewsBox && <NewsBox />}
+            {hasMessage &&
+                <button
+                    className="newsButton"
+                    onClick={toggleNewsBox}
+                > Last update
+                </button>}
+            {showNewsBox &&
+                <NewsBox
+                    newsBoxMessage={newsBoxMessage}
+                />}
         </div>
     );
 };
--- src/components/NewsBox.tsx
+++ src/components/NewsBox.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import 'styles/ViewBoxes.scss';
 
-const NewsBox: React.FC = () => {
+const NewsBox = ({ newsBoxMessage }: {
+    newsBoxMessage: string
+}) => {
 
     return (
-        <div className="viewBox newsBox">
-            <div>It's been a little while...</div>
-            <div>"AV" & "Platform" available to submit 🤓</div>
-            <br />
-            <div>Happy Swapping! 🎉</div>
+        <div
+            className="viewBox newsBox"
+        >
+            {newsBoxMessage}
         </div>
     );
 };
